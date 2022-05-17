b7ed524dece9123a2f596bba04ea5ff71af755e6
Derek Vogt <derekvogt3@gmail.com>
2022-05-17 12:39:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(
+      "Thank you, " + names[i] + ", for the wonderful " + event + " gift!"
+    );
+  }
+  return messages;
+}
+
+function countDown(number) {
+  for (let i = 0; i < number; i++) {
+    console.log(number - i);
+  }
+  console.log(0);
+}
